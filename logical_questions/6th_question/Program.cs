31cd7aab2c3f176e1652aafd888c2e37e3882277
rashid234 <85236492+rashid234@users.noreply.github.com>
2023-01-18 14:21:39 +0530

    12th

---
+++ logical_questions/6th_question/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] locations = new string[] {"pathiripala","palakkad","ernakulam","kannur"};
+
+
