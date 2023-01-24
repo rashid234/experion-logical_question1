d9b1956fcccdf4cebc0ec807a7b63332f26bec59
rashid234 <85236492+rashid234@users.noreply.github.com>
2023-01-24 22:20:25 +0530

    Delete logical_questions/6th_question directory

--- logical_questions/6th_question/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-string[] locations = new string[] {"pathiripala","palakkad","ernakulam","kannur"};
-
-
